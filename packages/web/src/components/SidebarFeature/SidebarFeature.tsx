96028e3047b74862e4524a300ebb1ad935e0ae6f
tunamaguro <tsu7magu6@gmail.com>
2022-06-28 21:36:30 +0900

    サイドバーのリンクのコンポーネント

---
+++ packages/web/src/components/SidebarFeature/SidebarFeature.tsx
@@ -0,0 +1,29 @@
+import { Flex, Icon, Text } from '@chakra-ui/react'
+import NextLink from 'next/link'
+import type { IconType } from 'react-icons'
+
+type SidebarFeatureProps = {
+  name: string
+  icon: IconType
+  to: string
+}
+export const SidebarFeature = ({ icon, name, to }: SidebarFeatureProps) => (
+  <NextLink href={to} passHref>
+    <Flex
+      as="a"
+      backgroundColor="primary.gray"
+      py="4"
+      pl="20"
+      fontSize="2xl"
+      _hover={{
+        backgroundColor: 'primary.black',
+        textColor: 'text.white100',
+      }}
+    >
+      <Flex minW="sm" alignItems="center">
+        <Icon as={icon} boxSize="24px" />
+        <Text ml="4">{name}</Text>
+      </Flex>
+    </Flex>
+  </NextLink>
+)
