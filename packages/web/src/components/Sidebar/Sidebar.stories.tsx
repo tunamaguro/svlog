5bbedff1f9a7384d9d4a8506503598dd9ea7a673
tunamaguro <tsu7magu6@gmail.com>
2022-06-29 21:03:51 +0900

    サイドバーを固定化+子要素を受け取れるように+サイドバーのmin-widthを指定

--- packages/web/src/components/Sidebar/Sidebar.stories.tsx
+++ packages/web/src/components/Sidebar/Sidebar.stories.tsx
@@ -1,17 +1,9 @@
-import { Box } from '@chakra-ui/react'
 import { ComponentMeta, ComponentStoryObj } from '@storybook/react'
 
 import { Sidebar } from './Sidebar'
 
 const meta: ComponentMeta<typeof Sidebar> = {
   component: Sidebar,
-  decorators: [
-    (Story) => (
-      <Box w="sm">
-        <Story />
-      </Box>
-    ),
-  ],
 }
 
 export default meta
--- packages/web/src/components/Sidebar/Sidebar.tsx
+++ packages/web/src/components/Sidebar/Sidebar.tsx
@@ -1,4 +1,5 @@
 import { Box, Flex, Spacer } from '@chakra-ui/react'
+import { ReactNode } from 'react'
 import { BiLogOut } from 'react-icons/bi'
 import { SiGoogleanalytics } from 'react-icons/si'
 import { TbClipboardText } from 'react-icons/tb'
@@ -7,23 +8,35 @@ import { VscGraphLine } from 'react-icons/vsc'
 import { Logo } from '../Logo'
 import { SidebarFeature } from '../SidebarFeature'
 
-export const Sidebar = () => (
-  <Flex
-    minH="100vh"
-    backgroundColor="primary.gray"
-    direction="column"
-    justifyContent="stretch"
-    alignItems="center"
-  >
-    <Logo my="16" />
-    <Box w="full" as="nav">
-      <SidebarFeature to="#" name="Dashboard" icon={SiGoogleanalytics} />
-      <SidebarFeature to="#" name="Records" icon={TbClipboardText} />
-      <SidebarFeature to="#" name="Weekly Analytics" icon={VscGraphLine} />
-    </Box>
-    <Spacer />
-    <Box w="full" pb="28">
-      <SidebarFeature to="#" name="Log out" icon={BiLogOut} />
+type SidebarProps = {
+  children: ReactNode
+}
+
+export const Sidebar = ({ children }: SidebarProps) => (
+  <Flex position="relative" direction="row" alignItems="stretch">
+    <Flex
+      position="sticky"
+      top="0"
+      h="100vh"
+      minW="sm"
+      backgroundColor="primary.gray"
+      direction="column"
+      justifyContent="stretch"
+      alignItems="center"
+    >
+      <Logo my="16" />
+      <Box w="full" as="nav">
+        <SidebarFeature to="#" name="Dashboard" icon={SiGoogleanalytics} />
+        <SidebarFeature to="#" name="Records" icon={TbClipboardText} />
+        <SidebarFeature to="#" name="Weekly Analytics" icon={VscGraphLine} />
+      </Box>
+      <Spacer />
+      <Box w="full" pb="28">
+        <SidebarFeature to="#" name="Log out" icon={BiLogOut} />
+      </Box>
+    </Flex>
+    <Box p="6" flexGrow="1">
+      {children}
     </Box>
   </Flex>
 )
